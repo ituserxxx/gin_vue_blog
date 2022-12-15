96c4e4d736d6f82485b349cfc59d0725e262468d
ituserxxx <ituserxxx>
2022-12-15 19:51:15 +0800

    as

--- admin/vue.config.js
+++ admin/vue.config.js
@@ -39,7 +39,7 @@ module.exports = {
     // before: require('./mock/mock-server.js'),
     proxy: { // 代理
       '/admin': {
-        target: 'http://127.0.0.1:6008', // 后台端口地址
+        target: 'http://0.0.0.0:6008', // 后台端口地址
         changeOrigin: true, // 将基于名称的虚拟托管网站的选项，如果不配置，请求会报404  如果接口跨域，需要进行这个参数配置
         ws: true, // true / false，是否代理websockets
         secure: false, // 如果是https接口，需要配置这个参数
--- vue-front/vue.config.js
+++ vue-front/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
     devServer: {
       proxy: {
         '/blog': {
-          target: 'http://127.0.0.1:6008',
+          target: 'http://0.0.0.0:6008',
           ws: true,
           changeOrigin: true
         },
