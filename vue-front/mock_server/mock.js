784f836bd85b153cc20e2074bc5960fd457a792c
ituserxxx <1034610091@qq.com>
2023-06-27 19:33:48 +0800

    add search input

---
+++ vue-front/mock_server/mock.js
@@ -0,0 +1,56 @@
+var http = require('http')
+// var querystring = require('querystring')
+http.createServer(function (request, response) {
+    response.setHeader('Access-Control-Allow-Origin', '*')
+    // 允许的header类型
+    response.setHeader('Access-Control-Allow-Headers', 'content-type')
+    // 跨域允许的请求方式
+    response.setHeader('Access-Control-Allow-Methods', 'DELETE,PUT,POST,GET,OPTIONS')
+    const url = request.url
+    let data = {}
+
+    // 2.注册data事件接收数据（每当收到一段表单提交的数据，该方法会执行一次）
+    request.on('data', function (chunk) {
+        data += chunk
+    })
+
+    request.on('end', function () {
+        data = decodeURI(data)
+        // console.log(data)
+        // var dataObject = querystring.parse(data)
+        // console.log(dataObject)
+    })
+
+    console.log('请求url=>' + request.url)
+    if (url === '/blog/article/list') {
+        data = postList()//此处就是接口返回的数据
+    }
+
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'application/json; charset=UTF-8'})
+    // 发送响应数据
+    response.end(JSON.stringify({
+        code: 20000,
+        msg: true,
+        data: data
+    }))
+}).listen(6008)
+
+function postList() {
+    return {
+        "article_list": [{
+            "id": 116,
+            "title": "wsl2 原生 Linux 安装 Docker 方式  (非windows 桌面版 docker )"
+        }, {
+            "id": 109,
+            "title": "nginx 部署网站"
+        }, {
+            "id": 108,
+            "title": "linux 查看当前监听的所有端口"
+        }, {
+            "id": 107,
+            "title": "mysql count(*)、count(1)、count(id)，count(name)比较"
+        }],
+        "total": 112
+    }
+}
--- vue-front/src/components/HeadContainer.vue
+++ vue-front/src/components/HeadContainer.vue
@@ -2,24 +2,31 @@
   <div class="HeadContainer">
     <el-header>
       <el-menu
-        class="el-menu-demo"
-        mode="horizontal"
-        active-text-color="#ffd04b"
-        background-color="#545c64"
-        text-color="#fff"
+          class="el-menu-demo"
+          mode="horizontal"
+          active-text-color="#ffd04b"
+          background-color="#545c64"
+          text-color="#fff"
       >
         <el-menu-item
-          target="_blank"
-          v-for="(val, index) in headCategoryList"
-          :key="index"
-          index="index"
+            target="_blank"
+            v-for="(val, index) in headCategoryList"
+            :key="index"
+            index="index"
         >
           <router-link :to="val.url">
             <span class="menu-title">{{ val.title }}</span>
           </router-link>
         </el-menu-item>
       </el-menu>
+
     </el-header>
+    <el-autocomplete
+        v-model="state"
+        :fetch-suggestions="querySearchAsync"
+        placeholder="请输入内容"
+        @select="handleSelect"
+    ></el-autocomplete>
   </div>
 </template>
 
@@ -29,17 +36,49 @@ export default {
   data() {
     return {
       headCategoryList: [
-        { title: "首页", url: "/Home" },
-        { title: "标签", url: "/Tag" },
-        { title: "关于", url: "/About" },
+        {title: "首页", url: "/Home"},
+        {title: "标签", url: "/Tag"},
+        {title: "关于", url: "/About"},
       ],
+      restaurants: [],
+      state: '',
+      timeout: null
     };
   },
-  created() {},
-  methods: {},
+  mounted() {
+    this.restaurants = this.loadAll();
+  },
+  methods: {
+    loadAll() {
+      return [
+        {"value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号"},
+        {"value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号"},
+        {"value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113"},
+        {"value": "泷千家(天山西路店)", "address": "天山西路438号"},
+      ]
+    }
+  },
+  querySearchAsync(queryString, cb) {
+    var restaurants = this.restaurants;
+    var results = queryString ? restaurants.filter(this.createStateFilter(queryString)) : restaurants;
+
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(() => {
+      cb(results);
+    }, 3000 * Math.random());
+  },
+  createStateFilter(queryString) {
+    return (state) => {
+      return (state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+    };
+  },
+  handleSelect(item) {
+    console.log(item);
+  },
 };
 </script>
 <style scoped>
+
 .el-header {
   background-color: #ffcc99;
   color: #ccffcc;
@@ -53,7 +92,17 @@ export default {
   right: 8px;
   top: 0;
 }
+
 .menu-title {
   font-size: 25px;
 }
+
+.HeadContainer >>> .el-autocomplete {
+  position: relative;
+  display: inline-block;
+  left: 80%;
+  z-index: 50;
+}
+
+
 </style>
