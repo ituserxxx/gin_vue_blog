13556361c0f5296c3fc969567c5edda2d70e9a7a
ituserxxx <ituserxxx@gmail.com>
2023-06-28 19:31:20 +0800

    as

--- vue-front/src/components/HeadContainer.vue
+++ vue-front/src/components/HeadContainer.vue
@@ -21,18 +21,27 @@
       </el-menu>
 
     </el-header>
+
     <el-autocomplete
         v-model="state"
+        maxlength="30"
         :fetch-suggestions="querySearchAsync"
-        placeholder="请输入内容"
-        @select="handleSelect"
-    ></el-autocomplete>
+        placeholder="搜索一下"
+        clearable="true">
+
+<!--        <template slot-scope="{ item }">-->
+<!--          <div class="name">{{ item.value }}</div>-->
+<!--          <span class="addr">{{ item.address }}</span>-->
+<!--        </template>-->
+    </el-autocomplete>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "HeadContainer",
+
   data() {
     return {
       headCategoryList: [
@@ -42,39 +51,40 @@ export default {
       ],
       restaurants: [],
       state: '',
-      timeout: null
+      timeout:  null
     };
-  },
-  mounted() {
-    this.restaurants = this.loadAll();
   },
   methods: {
     loadAll() {
       return [
-        {"value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号"},
-        {"value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号"},
-        {"value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113"},
-        {"value": "泷千家(天山西路店)", "address": "天山西路438号"},
-      ]
+        { "value": "三全鲜食（北新泾店）", "address": "长宁区新渔路144号" },
+        { "value": "Hot honey 首尔炸鸡（仙霞路）", "address": "上海市长宁区淞虹路661号" },
+        { "value": "新旺角茶餐厅", "address": "上海市普陀区真北路988号创邑金沙谷6号楼113" },
+        { "value": "泷千家(天山西路店)", "address": "天山西路438号" },
+      ];
+    },
+    querySearchAsync(queryString, cb) {
+      var restaurants = this.restaurants;
+      var results = queryString ? restaurants.filter(this.createStateFilter(queryString)) : restaurants;
+
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(() => {
+        cb(results);
+      }, 3000 * Math.random());
+    },
+    createStateFilter(queryString) {
+      return (state) => {
+        return (state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
+      };
+    },
+    handleSelect(item) {
+      console.log(item);
     }
   },
-  querySearchAsync(queryString, cb) {
-    var restaurants = this.restaurants;
-    var results = queryString ? restaurants.filter(this.createStateFilter(queryString)) : restaurants;
+  mounted() {
+    this.restaurants = this.loadAll();
+  }
 
-    clearTimeout(this.timeout);
-    this.timeout = setTimeout(() => {
-      cb(results);
-    }, 3000 * Math.random());
-  },
-  createStateFilter(queryString) {
-    return (state) => {
-      return (state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
-    };
-  },
-  handleSelect(item) {
-    console.log(item);
-  },
 };
 </script>
 <style scoped>
